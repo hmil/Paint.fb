4222a07c1773b015639ab8d6f3198062702e74ad
Hadrien MILANO <hadrien.milano@gmail.com>
2012-05-20 20:23:56 +0200

    ajout du fichier app.js

---
+++ public/app-dev/js/app.js
@@ -0,0 +1,13 @@
+var app = {
+	// Classes
+	Collections: {},
+	Models: {},
+	Views: {},
+	// Instances
+	collections: {},
+	models: {},
+	views: {},
+	init: function () {
+		// Initialisation de l'application ici
+	}
+};
