881696ac80ee7de341ba0f03d393359beb5e53ad
Hadrien MILANO <hadrien.milano@gmail.com>
2012-07-19 22:04:17 +0200

    Application du dessin sur le canvas

--- public/app-dev/js/models/PaintTool.js
+++ public/app-dev/js/models/PaintTool.js
@@ -7,19 +7,49 @@ define(['app', 'lib/backbone', 'lib/underscore', 'models/Tool'], function(app){
 		},
 		
 		initialize: function(){
+			this.path = new Array();
 		},
 		
 		onMousedown: function(e){
 			e.buffer.beginPath();
 			e.buffer.moveTo(e.x, e.y);
+			
+			this.path.push({x: e.x, y: e.y});
 		},
 		
-		onMouseup: function(e){
+		onMouseup: function(e){	
+			//initialisation du canvas
+			e.canvas.strokeStyle = e.buffer.strokeStyle;
+			
+			//Dessin du trajet
+			this.drawPath(e.canvas, this.path);
+			
+			//on réinitialise le tableau mémoire
+			this.path.length = 0;
+			
+			//Et on nettoie le buffer
+			e.clearBuf();
 		},
 		
 		onMousemove: function(e){
-			e.buffer.lineTo(e.x, e.y);
-			e.buffer.stroke();
+			//On ajoute la nouvelle coordonnée
+			this.path.push({x: e.x, y: e.y});
+			
+			//On nettoie le buffer
+			e.clearBuf();
+
+			//Et on redessine le chemin
+			this.drawPath(e.buffer, this.path);
+		},
+		
+		drawPath: function(ctx, path){
+			ctx.beginPath();
+			
+			for(var i = 0 ; i < this.path.length ; i++){
+				ctx.lineTo(this.path[i].x, this.path[i].y);
+			}
+			
+			ctx.stroke();
 		}
 	
 	});
