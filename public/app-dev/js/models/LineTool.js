9ee35399227c731d25919afaa89e6667e5d83029
Hadrien Milano <hadrien.milano@gmail.com>
2012-07-20 10:55:04 +0200

    Ajout de l'outil line

--- public/app-dev/js/models/LineTool.js
+++ public/app-dev/js/models/LineTool.js
@@ -7,6 +7,39 @@ define(['app', 'lib/backbone', 'lib/underscore', 'models/Tool'], function(app){
 		},
 		
 		initialize: function(){
+			this.p1 = {x: 0, y:0};
+		},
+		
+		onMousedown: function(e){	
+			this.p1 = {x: e.x, y: e.y};
+		},
+		
+		onMouseup: function(e){	
+			//initialisation du canvas
+			e.canvas.strokeStyle = e.buffer.strokeStyle;
+			
+			//Dessin du trajet
+			this.drawLine(e.canvas, this.p1, {x: e.x, y: e.y});
+			
+			//Et on nettoie le buffer
+			e.clearBuf();
+		},
+		
+		onMousemove: function(e){			
+			//On nettoie le buffer
+			e.clearBuf();
+
+			//Et on redessine la ligne
+			this.drawLine(e.buffer, this.p1, {x: e.x, y: e.y});
+		},
+		
+		drawLine: function(ctx, p1, p2){
+			ctx.beginPath();
+			
+			ctx.moveTo(p1.x, p1.y);
+			ctx.lineTo(p2.x, p2.y);
+			
+			ctx.stroke();
 		}
 	
 	});
